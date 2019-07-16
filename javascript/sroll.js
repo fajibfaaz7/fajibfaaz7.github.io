a012affe96059ca0717a10221c0244548f3f0e58
Fajib <49675506+imfaazi@users.noreply.github.com>
2019-07-17 04:14:47 +0530

    added move to top button

--- javascript/sroll.js
+++ javascript/sroll.js
@@ -1,5 +1,6 @@
 window.onscroll = function() {
   scrollFunction();
+  scrollFunction2();
 };
 
 function scrollFunction() {
@@ -13,4 +14,24 @@ function scrollFunction() {
     document.getElementById("navanim").style.backgroundColor = "rgba(0,0,0,0)";
     document.getElementById("cover-text").style.fontSize = "40px";
   }
+}
+
+function scrollFunction2() {
+  if (
+    document.body.scrollTop > 500 ||
+    document.documentElement.scrollTop > 500
+  ) {
+    document.getElementById("top-btn").style.width = "60px";
+    document.getElementById("top-btn").style.height = "60px";
+    // document.getElementById("top-btn").style.transition = "0.6s";
+    document.getElementById("top-btn").style.display = "block";
+  } else {
+    document.getElementById("top-btn").style.width = "0px";
+    document.getElementById("top-btn").style.height = "0px";
+    document.getElementById("top-btn").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
