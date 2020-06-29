82202255d5ecefbc6bdff941c1314246155406e0
Fajib <faazfajib7@gmail.com>
2020-06-29 10:16:12 +0530

    Update

--- javascript/sroll.js
+++ javascript/sroll.js
@@ -4,15 +4,27 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
+  const elements = document.getElementsByClassName("nav-link")
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navanim").style.padding = "10px 20px 10px 20px";
+    document.getElementById("navanim").style.padding = "5px 20px 5px 20px";
     document.getElementById("navanim").style.backgroundColor =
-      "rgba(30,39,46, 1)";
+      "#ffffff";
+    document.querySelector(".nav-brand").style.color = "#5f6368"
     document.getElementById("cover-text").style.fontSize = "60px";
+
+    Array.from(elements).forEach((element)=>{
+      element.style.color = "#5f6368"
+    })
+    document.getElementById("navanim").style.boxShadow = "0 1px 2px 0 rgba(60,64,67,.3), 0 2px 6px 2px rgba(60,64,67,.15)"
   } else {
     document.getElementById("navanim").style.padding = "40px 60px 20px 50px"; //top,right,bottom,left
     document.getElementById("navanim").style.backgroundColor = "rgba(0,0,0,0)";
     document.getElementById("cover-text").style.fontSize = "40px";
+    document.querySelector(".nav-brand").style.color = "#ffffff"
+    Array.from(elements).forEach((element)=>{
+      element.style.color = "#ffffff"
+    })
+    document.getElementById("navanim").style.boxShadow = "none"
   }
 }
 
